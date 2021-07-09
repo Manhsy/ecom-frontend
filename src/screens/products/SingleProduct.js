eed362ff91c262b9795c663c2ec596fba2af89db
Manhsy <63436025+Manhsy@users.noreply.github.com>
2021-07-08 18:01:03 -0700

    test

--- src/screens/products/SingleProduct.js
+++ src/screens/products/SingleProduct.js
@@ -18,7 +18,22 @@ const SingleProduct = (props)=>{
                         style = {styles.image}
                     />
                 </View>
+                <View style = {styles.contentContainer}>
+                    <H1 style = {styles.contentHeader}>{item.name}</H1>
+                    <Text style = {styles.contentText}>{item.brand}</Text>
+                </View>
+                {/*rich description and availability*/}
             </ScrollView>
+            <View style = {styles.bottomContainer}>
+                <Left>
+                    <Text style = {styles.price}>${item.price}</Text>
+                </Left>
+                <Right>
+                    <Button
+                        title = "Add"
+                    />
+                </Right>
+            </View>
         </Container>
     )
 }
@@ -36,6 +51,32 @@ const styles = StyleSheet.create({
     image: {
         width: '100%',
         height: 250
+    },
+    contentContainer:{
+        marginTop: 20,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    contentHeader:{
+        fontWeight: 'bold',
+        marginBottom: 20
+    },
+    contentText:{
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginBottom:20
+    },
+    bottomContainer:{
+        flexDirection: 'row',
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        backgroundColor: 'white'
+    },
+    price:{
+        fontSize: 24,
+        margin: 20,
+        color: 'red'
     }
 })
 
