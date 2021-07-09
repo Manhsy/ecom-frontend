c7c00e5260f0a585b248dca7e4b815f221ba0430
Manhsy <63436025+Manhsy@users.noreply.github.com>
2021-07-08 20:37:17 -0700

    cart

--- src/Navigators/Main.js
+++ src/Navigators/Main.js
@@ -1,80 +1,71 @@
-import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import {View} from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome'
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { View } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 //stack
-import HomeNavigator from './HomeNavigator'
-import CartNavigator from './CartNavigator';
+import HomeNavigator from "./HomeNavigator";
+import CartNavigator from "./CartNavigator";
 
-const Tab = createBottomTabNavigator(
+import CartIcon from "../shared/CartIcon";
 
-)
-const Main = ()=>{
-    return (
-        <Tab.Navigator 
-            initialRouteName = 'Home'
-            tabBarOptions = {{
-                keyboardHidesTab: true,
-                showLabels: false,
-                activeTintColor: '#e91e63'
-            }}
-        >
-            <Tab.Screen
-                name = 'Home'
-                component = {HomeNavigator}
-                options = {{
-                    tabBarIcon: ({color})=>(
-                        <Icon 
-                            name = "home"
-                            style = {{position: 'relative' }}
-                            color = {color}
-                            size = {30}
-                        />
-                    )
-                }}
+const Tab = createBottomTabNavigator();
+const Main = () => {
+  return (
+    <Tab.Navigator
+      initialRouteName="Home"
+      tabBarOptions={{
+        keyboardHidesTab: true,
+        showLabels: false,
+        activeTintColor: "#e91e63",
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={HomeNavigator}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Icon
+              name="home"
+              style={{ position: "relative" }}
+              color={color}
+              size={30}
             />
-            <Tab.Screen
-                name = 'Cart'
-                component = {CartNavigator}
-                options = {{
-                    tabBarIcon: ({color})=>(
-                        <Icon 
-                            name = "shopping-cart"
-                            color = {color}
-                            size = {30}
-                        />
-                    )
-                }}           
-            />
-            <Tab.Screen
-                name = 'Admin'
-                component = {HomeNavigator}
-                options = {{
-                    tabBarIcon: ({color})=>(
-                        <Icon 
-                            name = "cog"
-                            color = {color}
-                            size = {30}
-                        />
-                    )
-                }}           
-            />
-            <Tab.Screen
-                name = 'User'
-                component = {HomeNavigator}
-                options = {{
-                    tabBarIcon: ({color})=>(
-                        <Icon 
-                            name = "user"
-                            color = {color}
-                            size = {30}
-                        />
-                    )
-                }}           
-            />
-        </Tab.Navigator>
-    )
-}
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Cart"
+        component={CartNavigator}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <View>
+              <Icon name="shopping-cart" color={color} size={30} />
+              <CartIcon />
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Admin"
+        component={HomeNavigator}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Icon name="cog" color={color} size={30} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="User"
+        component={HomeNavigator}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <Icon name="user" color={color} size={30} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
 
 export default Main;
--- src/screens/cart/cart.js
+++ src/screens/cart/cart.js
@@ -1,31 +1,131 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React from "react";
+import {
+  View,
+  StyleSheet,
+  Dimensions,
+  Button,
+  touchableOpacity,
+} from "react-native";
+import {
+  Container,
+  Text,
+  Left,
+  Right,
+  H1,
+  ListItem,
+  Thumbnail,
+  Body,
+} from "native-base";
+import Icon from "react-native-vector-icons/FontAwesome";
+import { connect } from "react-redux";
+import * as actions from "../../redux/actions/cartActions";
 
-import {connect} from 'react-redux';
+var { height, width } = Dimensions.get("window");
 
-const Cart = (props)=>{
+const Cart = (props) => {
+  var totalPrice = 0;
 
-    return(
-        <View style = {{flex: 1}}>
-            {props.cartItems.map(x=>{
-                return(
-                    <Text>{x.product.name}</Text>
-                )
-            })}
-        </View>
-    )
-}
+  props.cartItems.forEach((cart) => {
+    return (totalPrice += cart.product.price);
+  });
 
-const mapStateToProps = (state)=>{
-    const {cartItems} = state;
-    return {
-        cartItems: cartItems
-    }
-}
+  return (
+    <Container>
+      {props.cartItems.length > 0 ? (
+        <Container>
+          <H1 style={{ alignSelf: "center" }}>Cart</H1>
+          {props.cartItems.map((data) => {
+            return (
+              <ListItem style={styles.listItem} key={Math.random()} avatar>
+                <Left>
+                  <Thumbnail
+                    source={{
+                      uri: data.product.image
+                        ? data.product.image
+                        : "https://i.insider.com/5b1702bd1ae66244008b4c94?width=750&format=jpeg&auto=webp",
+                    }}
+                  />
+                </Left>
+                <Body style={styles.body}>
+                  <Left>
+                    <Text>{data.product.name}</Text>
+                  </Left>
+                  <Right>
+                    <Text>${data.product.price}</Text>
+                  </Right>
+                </Body>
+              </ListItem>
+            );
+          })}
+          <View style={styles.bottomContainer}>
+            <Left>
+              <Text style={styles.price}>${totalPrice.toFixed(2)}</Text>
+            </Left>
+            <Right>
+              <Button title="Clear" onPress={() => props.clearCart()} />
+            </Right>
+            <Right>
+              <Button
+                title="Checkout"
+                onPress={() => {
+                  props.navigation.navigate("Checkout");
+                }}
+              />
+            </Right>
+          </View>
+        </Container>
+      ) : (
+        <Container style={styles.emptyContainer}>
+          <Text>There are currently no item in cart</Text>
+        </Container>
+      )}
+    </Container>
+  );
+};
+const mapDispatchToProps = (dispatch) => {
+  return {
+    clearCart: () => dispatch(actions.clearCart()),
+  };
+};
+
+const mapStateToProps = (state) => {
+  const { cartItems } = state;
+  return {
+    cartItems: cartItems,
+  };
+};
 
 const styles = StyleSheet.create({
+  emptyContainer: {
+    height: height,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  listItem: {
+    alignItems: "center",
+    backgroundColor: "white",
+    justifyContent: "center",
+  },
+  body: {
+    margin: 10,
+    alignItems: "center",
+    flexDirection: "row",
+  },
+  bottomContainer: {
+    flexDirection: "row",
+    position: "absolute",
+    bottom: 0,
+    left: 0,
+    backgroundColor: "white",
+    elevation: 20,
+  },
+  price: {
+    fontSize: 18,
+    margin: 20,
+    color: "red",
+  },
+  checkout: {},
+});
 
-})
-
-//first params adds function props
-export default connect(mapStateToProps, null)(Cart);
+//connect(state, dispatch)
+export default connect(mapStateToProps, mapDispatchToProps)(Cart);
