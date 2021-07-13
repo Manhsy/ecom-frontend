fe92b3509b6dd91f57483db1795272c05e5e04fe
Manhsy <63436025+Manhsy@users.noreply.github.com>
2021-07-12 21:11:38 -0700

    fixed searched product

--- src/screens/products/ProductContainer.js
+++ src/screens/products/ProductContainer.js
@@ -115,7 +115,7 @@ const ProductContainer = (props) => {
           {focus == true ? (
             <SearchedProduct
               navigation={props.navigation}
-              productFiltered={changeCategory}
+              productsFiltered={productFiltered}
             />
           ) : (
             <ScrollView>
--- src/screens/products/SearchedProducts.js
+++ src/screens/products/SearchedProducts.js
@@ -1,52 +1,54 @@
-import React from 'react';
-import {View, StyleSheet, Dimensions, FlatList} from 'react-native';
-import {Container, Left, Body, ListItem, Thumbnail, Text} from 'native-base';
+import React from "react";
+import { View, StyleSheet, Dimensions, FlatList } from "react-native";
+import { Container, Left, Body, ListItem, Thumbnail, Text } from "native-base";
 
-var {width} = Dimensions.get("window")
+var { width } = Dimensions.get("window");
 
 const SearchedProduct = (props) => {
-    const { productFiltered } = props;
-    return(
-        <Container style={{ width: width }}>
-            {productFiltered.length > 0 ?  (
-                productFiltered.map((item) => (
-                    <ListItem
-                        onPress = {()=>{
-                            props.navigation.navigate('ProductDetail', {item: item})
-                        }}
-                        key={item._id.$oid}
-                        avatar
-                    >
-                        <Left>
-                            <Thumbnail 
-                                source={{uri: item.image ? 
-                                    item.image : 'https://cdn.pixabay.com/photo/2012/04/01/17/29/box-23649_960_720.png'
-                                        }}
-                            />
-                        </Left>
-                        <Body>
-                            <Text>{item.name}</Text>
-                            <Text note>{item.description}</Text>
-                        </Body>
-                    </ListItem>
-                ))
-            ) : (
-                <View style={styles.center}>
-                    <Text style={{ alignSelf:  'center' }}>
-                        No products match the selected criteria
-                    </Text>
-                </View>
-            )}
-        </Container>
-    );
+  const { productsFiltered } = props;
+  return (
+    <Container style={{ width: width }}>
+      {productsFiltered.length > 0 ? (
+        productsFiltered.map((item) => (
+          <ListItem
+            onPress={() => {
+              props.navigation.navigate("ProductDetail", { item: item });
+            }}
+            key={item._id.$oid}
+            avatar
+          >
+            <Left>
+              <Thumbnail
+                source={{
+                  uri: item.image
+                    ? item.image
+                    : "https://cdn.pixabay.com/photo/2012/04/01/17/29/box-23649_960_720.png",
+                }}
+              />
+            </Left>
+            <Body>
+              <Text>{item.name}</Text>
+              <Text note>{item.description}</Text>
+            </Body>
+          </ListItem>
+        ))
+      ) : (
+        <View style={styles.center}>
+          <Text style={{ alignSelf: "center" }}>
+            No products match the selected criteria
+          </Text>
+        </View>
+      )}
+    </Container>
+  );
 };
 
 const styles = StyleSheet.create({
-    center: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: 100
-    }
-})
+  center: {
+    justifyContent: "center",
+    alignItems: "center",
+    height: 100,
+  },
+});
 
 export default SearchedProduct;
